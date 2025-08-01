51086e06f436dad16c270d5e0c06a747ac504696
jirayuwongtom <lovelava600@gmail.com>
2025-08-01 17:08:11 +0700

    submit lab & output

---
+++ Lab/lab6.cpp
@@ -0,0 +1,25 @@
+#include <stdio.h>
+int main(){
+    int n;
+    printf("Enter value : ");
+    if(scanf("%d", &n) !=1){
+        printf("Please enter numbers only.");
+        return 1;
+    }//end if
+
+    if(n % 2 ==1){
+        for(int i = 0; i<= n; i++){
+            if( i % 2 == 1){
+                printf("%d ", i);
+            }//end if
+        }//end for
+    }//end if
+    else{
+        for (int i = n; i >= 0; i--){
+            if (i % 2 ==0){
+                printf("%d ", i);
+            } //end if
+        }//end for
+    }//end if
+    return 0;
+}//end function
